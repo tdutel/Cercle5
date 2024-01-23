9bf13ec28bdd7cf1d5cd631abe78c0b4085ddddb
Timothe Dutel <tdutel@student.42lyon.fr>
2024-01-23 15:50:17 +0100

    rm unused headers

--- CPP/CPP_Module_05/ex01/srcs/Form.cpp
+++ CPP/CPP_Module_05/ex01/srcs/Form.cpp
@@ -6,12 +6,11 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 12:15:34 by tdutel            #+#    #+#             */
-/*   Updated: 2024/01/23 14:51:30 by tdutel           ###   ########.fr       */
+/*   Updated: 2024/01/23 15:49:47 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"../headers/Form.hpp"
-#include <iomanip>
 
 /* Constructor Destructor */
 
