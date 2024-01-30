5dc3e98d09140ada73d73dc384ee5b4f31cf7e1a
Timothe Dutel <tdutel@student.42lyon.fr>
2024-01-30 10:15:47 +0100

    done ex02

--- CPP/CPP_Module_06/ex02/main.cpp
+++ CPP/CPP_Module_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 09:15:47 by tdutel            #+#    #+#             */
-/*   Updated: 2024/01/30 09:38:24 by tdutel           ###   ########.fr       */
+/*   Updated: 2024/01/30 10:15:01 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 #include"headers/A.hpp"
 #include"headers/B.hpp"
 #include"headers/C.hpp"
-#include <ctime>
 
 #include <iostream>
+#include <ctime>
 
 Base	*generate(void)
 {
@@ -27,27 +27,75 @@ Base	*generate(void)
 	{
 	case 0:
 		p = new A;
-		std::cout << "A ";
+		std::cout << "generating an A object" << std::endl;
 		break;
 	case 1:
 		p = new B;
-		std::cout << "B ";
+		std::cout << "generating a B object" << std::endl;
 		break;
 	case 2:
 		p = new C;
-		std::cout << "C ";
+		std::cout << "generating a C object" << std::endl;
 	}
 	return (p);
 }
 
 void	identify(Base *p)
 {
-	std::cout << p << std::endl;
+	A	*tmpA;
+	tmpA = dynamic_cast<A*>(p);
+	if (tmpA != NULL)
+	{
+		std::cout << "type is A" << std::endl;
+		return;
+	}
+	B	*tmpB = dynamic_cast<B*>(p);
+	if (tmpB != NULL)
+	{
+		std::cout << "type is B" << std::endl;
+		return;
+	}
+	C	*tmpC = dynamic_cast<C*>(p);
+	if (tmpC != NULL)
+	{
+		std::cout << "type is C" << std::endl;
+		return;
+	}
+}
+
+void	identify(Base &p)
+{
+	try
+	{
+		A	&tmpA = dynamic_cast<A&>(p);
+		std::cout << "type is A" << std::endl;
+	}
+	catch(std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		B	&tmpB = dynamic_cast<B&>(p);
+		std::cout << "type is B" << std::endl;
+	}
+	catch(std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		C	&tmpC = dynamic_cast<C&>(p);
+		std::cout << "type is C" << std::endl;
+	}
+	catch(std::bad_cast &bc)
+	{
+	}
+
 }
 
 int	main(void)
 {
 	Base *a = generate();
 	identify(a);
+	identify(*a);
 	delete (a);
 }
