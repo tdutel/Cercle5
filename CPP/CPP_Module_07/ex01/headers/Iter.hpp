56d688b84bd87148ef2d3de0756cc97875757f90
Timothe Dutel <tdutel@student.42lyon.fr>
2024-01-30 13:53:22 +0100

    ex01 probably done

---
+++ CPP/CPP_Module_07/ex01/headers/Iter.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 12:09:22 by tdutel            #+#    #+#             */
+/*   Updated: 2024/01/30 13:48:53 by tdutel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+
+template <typename T>
+void	print(T x)
+{
+	std::cout << *x << std::endl;
+};
+
+template <typename T>
+void	iter(T &tab, unsigned int len, void(*print)(T x))
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		std::cout << "tab[" << i << "] : ";
+		print(&tab[i]);
+	}
+};
+
+#endif
