b523be4b912daa77bef6fe42d088e5bc918e4c0b
Timothe Dutel <tdutel@student.42lyon.fr>
2024-01-30 15:00:20 +0100

    ex02 first approach

---
+++ CPP/CPP_Module_07/ex02/headers/Array.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 13:59:19 by tdutel            #+#    #+#             */
+/*   Updated: 2024/01/30 14:50:40 by tdutel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+class Array
+{
+private:
+	/* data */
+public:
+	Array(void);
+	Array(unsigned int n);
+	~Array();
+};
+
+Array::Array(/* args */)
+{
+}
+
+Array::~Array()
+{
+}
+
+
+#endif
