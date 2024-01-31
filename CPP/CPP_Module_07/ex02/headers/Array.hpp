bbf73d8c888691ad78c80894cc158c8457ffafdd
Timothe Dutel <tdutel@student.42lyon.fr>
2024-01-31 10:54:29 +0100

    1st approach ex02

--- CPP/CPP_Module_07/ex02/headers/Array.hpp
+++ CPP/CPP_Module_07/ex02/headers/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 13:59:19 by tdutel            #+#    #+#             */
-/*   Updated: 2024/01/30 14:50:40 by tdutel           ###   ########.fr       */
+/*   Updated: 2024/01/31 10:52:02 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,87 @@
 
 # include <iostream>
 
+template <typename T>
 class Array
 {
 private:
-	/* data */
+	T				*_tab;
+	unsigned int	_size;
+
 public:
-	Array(void);
-	Array(unsigned int n);
-	~Array();
+
+// constructor //
+
+	Array(void) : _size(0)
+	{
+		this->_tab = new T();
+		std::cout << "Constructor called" << std::endl;
+	};
+
+	Array(const Array &cpy) : _size(cpy._size)
+	{
+		if (this->_size != 0)
+		{
+			this->_tab = new T[this->_size];
+				for (size_t i = 0; i != this->_size; i++)
+					this->_tab[i] = cpy._tab[i];
+			std::cout << "Copy Constructor called" << std::endl;
+		}
+	};
+
+	Array(unsigned int n) : _size(n)
+	{
+		if (this->_size == 0)
+			this->_tab = NULL;
+		else
+		{
+
+			this->_tab = new T[n];
+			for (size_t i = 0; i != this->_size; i++)
+					this->_tab[i] = T();
+		}
+		std::cout << "Constructor Parametric called" << std::endl;
+	};
+
+	~Array()
+	{
+		delete[] this->_tab;
+		std::cout << "Destructor called" << std::endl;
+	};
+
+// operator //
+
+	Array &operator=(const Array& other)
+{
+	if (this->_size > 0)
+		delete[] this->_tab;
+	else
+		delete this->_tab;
+	this->_size = other._size;
+	this->_tab = new T[this->_size];
+	for (size_t i = 0; i < this->_size; i++)
+	{
+		this->_tab[i] = other._tab[i];
+	}
+	std::cout << "Copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+	T	&operator[](const unsigned int i)
+	{
+		if (i >= this->_size)
+		{
+			throw std::exception();
+		}
+		return (this->_tab[i]);
+	}
+
+// functions //
+	unsigned int	size(void)
+	{
+		return (this->_size);
+	};
+	
 };
-
-Array::Array(/* args */)
-{
-}
-
-Array::~Array()
-{
-}
-
 
 #endif
--- CPP/CPP_Module_07/ex02/main.cpp
+++ CPP/CPP_Module_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 13:58:35 by tdutel            #+#    #+#             */
-/*   Updated: 2024/01/30 14:49:56 by tdutel           ###   ########.fr       */
+/*   Updated: 2024/01/31 10:53:34 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,29 @@
 
 int	main()
 {
-	int * a = new int();
-	std::cout << *a;
+	try
+	{
+		Array<int>	a(10);
+		Array<int>	b;
+		std::cout << std::endl << "a size : " << a.size() << std::endl;
+		std::cout << "b size : " << b.size() << std::endl << std::endl;
+		for (size_t i = 0; i < a.size(); i++)
+		{
+			a[i] = i;
+			// b[b.size() - i - 1] = i;
+			std::cout << "a[" << i << "] : " << a[i] << std::endl;
+		}
+		std::cout << std::endl;
+		b = a;
+		std::cout << std::endl;
+		for (size_t i = 0; i < b.size(); i++)
+		{
+			std::cout << "b[" << i << "] : " << b[i] << std::endl;
+		}
+		std::cout << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
