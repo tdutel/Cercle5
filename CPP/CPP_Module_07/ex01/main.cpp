8eb727ea57536737f1b3b94d537a54846a0a357c
Timothe Dutel <tdutel@student.42lyon.fr>
2024-02-07 09:07:21 +0100

    done module

--- CPP/CPP_Module_07/ex01/main.cpp
+++ CPP/CPP_Module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 12:09:52 by tdutel            #+#    #+#             */
-/*   Updated: 2024/02/05 15:09:32 by tdutel           ###   ########.fr       */
+/*   Updated: 2024/02/07 09:00:38 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,16 @@ void	tolower(T& element){
 
 int	main()
 {
-	std::cout << "int tab : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}" << std::endl;
+	std::cout << "int tab : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}" << std::endl << std::endl;
 	int tab[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	iter<int, size_t, void>(tab, 10, &print);
 	std::cout << std::endl;
 	
-	std::cout << "char tab : \"eh yoo welcome\"" << std::endl;
+	std::cout << "char tab : \"eh yoo welcome\"" << std::endl << std::endl;
 	char chab[] = "eh yoo welcome";
 	iter<char, size_t, void>(chab, 14, &print);
 	std::cout << std::endl;
-	std::cout << "Now with tolower function :" << std::endl;
+	std::cout << "Now with tolower function :" << std::endl << std::endl;
 	iter<char, size_t, void>(chab, 14, &tolower);
 	return (0);
 }
