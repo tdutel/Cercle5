a29bab50872d8bc8cb53dababba6cefee4e84d90
Timothe Dutel <tdutel@student.42lyon.fr>
2024-02-07 11:55:45 +0100

    return value

--- CPP/CPP_Module_08/ex00/headers/easyfind.hpp
+++ CPP/CPP_Module_08/ex00/headers/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 11:31:14 by tdutel            #+#    #+#             */
-/*   Updated: 2024/02/02 13:15:55 by tdutel           ###   ########.fr       */
+/*   Updated: 2024/02/07 11:55:18 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,21 @@
 # include <algorithm>
 
 template < typename T>
-void	easyfind(T &ctn, int nb)
+int	easyfind(T &ctn, int nb)
 {
 	typename T::const_iterator it = ctn.begin();
 	typename T::const_iterator ite = ctn.end();
 	typename T::const_iterator result = std::find(it, ite, nb);
+
 	if (result == ite)
-	{
 		throw (std::exception());
+	int	i = 0;
+	while (it != result)
+	{
+		it++;
+		i++;
 	}
-	std::cout << "occurence " << *result << " found at position : " << std::distance(it, result) << std::endl;
-	// return(result);
+	return(i);
 }
 
 #endif
--- CPP/CPP_Module_08/ex00/main.cpp
+++ CPP/CPP_Module_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 11:32:29 by tdutel            #+#    #+#             */
-/*   Updated: 2024/02/07 09:11:00 by tdutel           ###   ########.fr       */
+/*   Updated: 2024/02/07 11:53:27 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,21 @@ int	main(void)
 		std::vector<int> a;
 		a.assign(&temp[0], &temp[8]);
 		std::cout << "find the occurence 3 in a." << std::endl;
-		easyfind(a, 3);
-		std::cout << std::endl;
+		std::cout << "occurence found at position : " << easyfind(a, 3) << std::endl << std::endl;
 
 		char tmp2[] = { "hello everyone and welcome." };
 		std::cout << "create \x1b[32mvector\033[0m char b = {\"" << tmp2 << "\"}" << std::endl;
 		std::vector<char> b;
 		b.assign(&tmp2[0], &tmp2[26]);
 		std::cout << "find the occurence 'w' in b." << std::endl;
-		easyfind(b, 'w');
-		std::cout << std::endl;
+		std::cout << "occurence found at position : " << easyfind(b, 'w') << std::endl << std::endl;
 
 		int tmp3[] = { 1, 2, 3 };
 		std::cout << "create \x1b[32mvector\033[0m int err = { 1, 2, 3 }" << std::endl;
 		std::vector<int> err;
 		err.assign(&tmp3[0], &tmp3[2]);
 		std::cout << "find the occurence 4 in err." << std::endl;
-		easyfind(err, 4);
-		std::cout << std::endl;
+		std::cout << easyfind(err, 4) << std::endl << std::endl;
 	}
 	catch(const std::exception& e)
 	{
@@ -58,24 +55,21 @@ int	main(void)
 		std::list<int> a;
 		a.assign(&temp[0], &temp[8]);
 		std::cout << "find the occurence 3 in a." << std::endl;
-		easyfind(a, 3);
-		std::cout << std::endl;
+		std::cout << "occurence found at position : " << easyfind(a, 3) << std::endl << std::endl;
 
 		char tmp2[] = { "hello everyone and welcome." };
 		std::cout << "create \x1b[33mlist\033[0m char b = {\"" << tmp2 << "\"}" << std::endl;
 		std::list<char> b;
 		b.assign(&tmp2[0], &tmp2[26]);
 		std::cout << "find the occurence 'w' in b." << std::endl;
-		easyfind(b, 'w');
-		std::cout << std::endl;
+		std::cout << "occurence found at position : " << easyfind(b, 'w') << std::endl << std::endl;
 
 		int tmp3[] = { 1, 2, 3 };
 		std::cout << "create \x1b[33mlist\033[0m int err = { 1, 2, 3 }" << std::endl;
 		std::list<int> err;
 		err.assign(&tmp3[0], &tmp3[2]);
 		std::cout << "find the occurence 4 in err." << std::endl;
-		easyfind(err, 4);
-		std::cout << std::endl;
+		std::cout << easyfind(err, 4) << std::endl << std::endl;
 	}
 	catch(const std::exception& e)
 	{
@@ -90,24 +84,21 @@ int	main(void)
 		std::deque<int> a;
 		a.assign(&temp[0], &temp[8]);
 		std::cout << "find the occurence 3 in a." << std::endl;
-		easyfind(a, 3);
-		std::cout << std::endl;
+		std::cout << "occurence found at position : " << easyfind(a, 3) << std::endl << std::endl;
 
 		char tmp2[] = { "hello everyone and welcome." };
 		std::cout << "create \x1b[34mdeque\033[0m char b = {\"" << tmp2 << "\"}" << std::endl;
 		std::deque<char> b;
 		b.assign(&tmp2[0], &tmp2[26]);
 		std::cout << "find the occurence 'w' in b." << std::endl;
-		easyfind(b, 'w');
-		std::cout << std::endl;
+		std::cout << "occurence found at position : " << easyfind(b, 'w') << std::endl << std::endl;
 
 		int tmp3[] = { 1, 2, 3 };
 		std::cout << "create \x1b[34mdeque\033[0m int err = { 1, 2, 3 }" << std::endl;
 		std::deque<int> err;
 		err.assign(&tmp3[0], &tmp3[2]);
 		std::cout << "find the occurence 4 in err." << std::endl;
-		easyfind(err, 4);
-		std::cout << std::endl;
+		std::cout << easyfind(err, 4) << std::endl << std::endl;
 	}
 	catch(const std::exception& e)
 	{
