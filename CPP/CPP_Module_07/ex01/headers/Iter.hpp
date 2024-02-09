1f527e0ebac8ea963bfca5ef90cb132062ffc199
Timothe Dutel <tdutel@student.42lyon.fr>
2024-02-09 12:18:09 +0100

    add template for const elements

--- CPP/CPP_Module_07/ex01/headers/Iter.hpp
+++ CPP/CPP_Module_07/ex01/headers/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 12:09:22 by tdutel            #+#    #+#             */
-/*   Updated: 2024/02/05 15:07:48 by tdutel           ###   ########.fr       */
+/*   Updated: 2024/02/09 12:15:53 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,16 @@
 
 template <typename T, typename U, typename V>
 void	iter(T *tab, U len, V (*f)(T&))
+{
+	for (U i = 0; i < len; ++i)
+	{
+		std::cout << "tab[" << i << "] : ";
+		f(tab[i]);
+	}
+};
+
+template <typename T, typename U, typename V>
+void	iter(const T *tab, const U len, V (*f)(const T&))
 {
 	for (U i = 0; i < len; ++i)
 	{
--- CPP/CPP_Module_07/ex01/main.cpp
+++ CPP/CPP_Module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 12:09:52 by tdutel            #+#    #+#             */
-/*   Updated: 2024/02/07 09:00:38 by tdutel           ###   ########.fr       */
+/*   Updated: 2024/02/09 12:16:56 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,34 @@ int	main()
 	return (0);
 }
 
+// class Awesome
+// {
+//   public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//   private:
+//     int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+// {
+//   o << rhs.get();
+//   return o;
+// }
+
+// template< typename T >
+// void print( T& x )
+// {
+//   std::cout << x << std::endl;
+//   return;
+// }
+
+// int main() {
+//   int tab[] = { 0, 1, 2, 3, 4 };
+//   Awesome tab2[5];
+
+//   iter( tab, 5, print<const int> );
+//   iter( tab2, 5, print<Awesome> );
+
+//   return 0;
+// }
