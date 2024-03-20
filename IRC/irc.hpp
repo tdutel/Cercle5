f19fa6e3370469033da321e222825371a7966d56
Timothe Dutel <tdutel@student.42lyon.fr>
2024-03-20 11:30:34 +0100

    add hpp

---
+++ IRC/irc.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   irc.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 11:07:04 by tdutel            #+#    #+#             */
+/*   Updated: 2024/03/20 11:22:40 by tdutel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IRC_HPP
+# define IRC_HPP
+
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <unistd.h>
+# include <string.h>
+# include <vector>
+# include <iostream>
+
+#include <cstring>
+
+# include <cstdlib>
+# include <iomanip>
+
+#include <stdio.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+
+#endif
